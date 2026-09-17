fbb219bf2e8e7fc7c694a50a25dcea089e41ebe6
FrederikTheDuck <145444669+TheDuckFrederik@users.noreply.github.com>
2026-09-17 20:56:35 +0200

    17-09-2026

---
+++ 1r/DWES/htdocs/ctso.js
@@ -0,0 +1,13 @@
+//
+prompt("What is your name?\n")
+prompt("What is your age?\n")
+//
+let num1 = "2";
+let num2 = 2; 
+//
+alert("Anyways, wanna se something cool?")
+num1 = prompt("You have no say in this, insert a number\n")
+num2 = prompt("Now another\n")
+alert("Watch and learn.")
+alert(num1 + " + " + num2 + " = " + (num1 + num2))
+//
---
+++ 1r/DWES/htdocs/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DWES</title>
+    <!-- Quack -->
+</head>
+<style>
+    body {
+    font-family: "Comic Sans MS", cursive, sans-serif;
+    }
+</style>
+<body>
+    <h1>DWES</h1>
+
+    <?php
+        $name = "Unai";
+
+        echo "<p>Hi <strong>Mom</strong>! My name is $name</p>";
+        echo "<p>Hi, my name is " . $name . "</p>"; // Dots to concatenate, bylat.
+        var_dump($name);
+
+        $randNum = rand(0,100);
+        echo "<h2>And today's random number is: $randNum</h2>";
+
+    ?>
+
+    <!-- <img src="https://thumb.wikimedia.org/wikipedia/commons/thumb/3/31/Webysther_20160423_-_Elephpant.svg/3840px-Webysther_20160423_-_Elephpant.svg.png?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=thumbnail"> -->
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi eligendi laborum, nemo cupiditate cumque optio autem? At laudantium mollitia illum, nobis, aliquid ipsam ad totam recusandae deserunt quod iure nesciunt omnis quidem autem. Consequatur eos assumenda corporis?</p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe unde esse tempore pariatur? Facere, quas! Veritatis debitis perspiciatis tenetur, tempore ea illum necessitatibus officiis deserunt voluptatem ipsum eum quae inventore, fuga facilis iure eaque. Quia tempore quaerat porro provident ab temporibus nemo nihil unde quidem debitis facilis non sint recusandae amet vero id harum accusamus repudiandae mollitia, et earum? Illum numquam libero deleniti maiores sapiente sit, itaque officiis, dolores quae adipisci facilis consequatur?</p>
+    
+</body>
+</html>
