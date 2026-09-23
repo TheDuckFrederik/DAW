7c788e5b4cebe4ebcf2b4ccd21e38135c37a0186
FrederikTheDuck <145444669+TheDuckFrederik@users.noreply.github.com>
2026-09-23 19:28:28 +0200

    22-09-2026

--- 1r/BD/AEA1/RA1/DB_INSTI.sql
+++ 1r/BD/AEA1/RA1/DB_INSTI.sql
@@ -14,10 +14,50 @@ CREATE TABLE IF NOT EXISTS materies (
     nom VARCHAR(100),
     hores TINYINT UNSIGNED NULL
 );
+-- 
+CREATE TABLE IF NOT EXISTS cursos (
+    idCurs INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    idEstudi VARCHAR(10) NOT NULL,
+    CONSTRAINT idEstudi
+    FOREIGN KEY (idEstudi)
+    REFERENCES estudis(idEstudi),
+	idMateria INT,
+    CONSTRAINT idMateria
+    FOREIGN KEY (idMateria)
+    REFERENCES materies(idMateria)
+);
 --
 INSERT INTO estudis (idEstudi, nom)
 VALUES 
 ("DAW", "Desenvolupament d'aplicacions Web"),
 ("DAM", "Desenvolupament d'aplicacions Multiplataforma"),
 ("ASIX", "Administracio de Sistemes Informatics en Xarxes");
+-- 
+INSERT INTO materies (codiMateria, nom, hores)
+VALUES
+("SASP", "Sostenibilitat aplicada al sistema productiu", NULL),
+("ED", "Entorns de desenvolupament", 99),
+("PRO", "Programació", NULL),
+("BD", "Bases de dades", NULL),
+("SI", "Sistemes informàtics", NULL),
+("IPO1", "Itinerari professional per a l'ocupabilitat 1", NULL),
+("DASP", "Digitalització aplicada als sistemes productius", NULL),
+("LMSGI", "Llenguatges de marques i sistemes de gestió de la informació", NULL),
+("DWES", "Desenvolupament web en entorn servidor", NULL),
+("DIW", "Disseny d'interfcies web", NULL);
+-- 
+INSERT INTO cursos (idEstudi, idMateria)
+VALUES
+("ASIX", 4),
+("DAM", 4),
+("DAW", 1),
+("DAW", 2),
+("DAW", 3),
+("DAW", 4),
+("DAW", 5),
+("DAW", 6),
+("DAW", 7),
+("DAW", 8),
+("DAW", 9),
+("DAW", 10);
 -- 
